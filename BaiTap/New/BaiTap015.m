ee98e70d50d6a95e454b766c93ba927038620019
Khai Tran <tqkhai@users.noreply.github.com>
2017-10-23 15:30:24 +0700

    Update Bài tập 015

---
+++ BaiTap/New/BaiTap015.m
@@ -0,0 +1,28 @@
+function BaiTap015()
+    %% Load Data Train
+    strData = 'train-images.idx3-ubyte';
+    strDataLabel = 'train-labels.idx1-ubyte';
+    
+    [imgDataTrain, lblDataTrain] = loadData(strData, strDataLabel);
+    
+    %% Extract Features
+    featuresDataTrain = ExtractFeaturesHog(imgDataTrain);
+    
+    %% Build model KNN
+    Mdl = fitcknn(featuresDataTrain', lblDataTrain);
+    
+    %% Load Data Test
+    strData = 't10k-images.idx3-ubyte';
+    strDataLabel = 't10k-labels.idx3-ubyte';
+    
+    [imgDataTest, lblActualDataTest] = loadData(strData, strDataLabel);
+    
+    %% Extract Features
+    featuresDataTest = ExtractFeaturesHog(imgDataTest);
+    
+    %% Save Result
+    lblResult = predict(Mdl, featuresDataTest');
+    nResult = (lblResult == lblActualDataTest);
+    nCount = sum(nResult);
+    fprintf('\nSo luong mau dung: %d\n', nCount);
+end
