f54acb8fbb9a6237a6517da49d8c425f41ffa4b7
Khai Tran <tqkhai@users.noreply.github.com>
2017-12-30 20:31:43 +0700

    Báo cáo đồ án cuối kỳ

--- BaoCaoCuoiKy/SourceCode/ExtractFeaturesLBP.m
+++ BaoCaoCuoiKy/SourceCode/ExtractFeaturesLBP.m
@@ -2,7 +2,7 @@ function [featuresDataTrain] = ExtractFeaturesLBP(imgDataTrain, numNeighbors, ra
     
     imgI1D = imgDataTrain(:, 1);
     imgI2D = reshape(imgI1D, 28, 28);
-    featureVector = extractLBPFeatures(imgI2D);
+    featureVector = extractLBPFeatures(imgI2D, 'NumNeighbors', numNeighbors, 'Radius', radius, 'CellSize', [cellSize cellSize], 'Normalization', normalization);
     
     nSize = length(featureVector);
     nTrainData = size(imgDataTrain, 2);
@@ -11,6 +11,6 @@ function [featuresDataTrain] = ExtractFeaturesLBP(imgDataTrain, numNeighbors, ra
     for i = 1 : nTrainData
         imgI1D = imgDataTrain(:, i);
         imgI2D = reshape(imgI1D, 28, 28);
-        featuresDataTrain(:, i) = extractLBPFeatures(imgI2D);
+        featuresDataTrain(:, i) = extractLBPFeatures(imgI2D, 'NumNeighbors', numNeighbors, 'Radius', radius, 'CellSize', [cellSize cellSize], 'Normalization', normalization);
     end
 end
